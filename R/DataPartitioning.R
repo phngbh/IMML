cccef43be13f462dcf313d5f1d247864e2931e1c
willy <wglaas@web.de>
2023-08-31 14:17:02 +0200

    DataPartitioning now returns all three lists, more detailed documentation

--- R/DataPartitioning.R
+++ R/DataPartitioning.R
@@ -1,162 +1,150 @@
 #' Data Partitioning
 #'
-#' @description Builds a data partition for the provided sets of IDs.
+#' @description Builds a list data partitions for the provided sets of IDs.
 #'
-#' @param phenotypeIDs A table holding the clinical IDs with a variable indicating,
-#' if the disease occurred or not.
-#' @param dataIDs A table holding all the clinical IDs with the respective IDs
-#' for each modality.
-#' @param partitioning A value, that signals what percentage of IDs are used for
-#' training in the feature selection.
-#' @param type This variable signals, if the training or testing set of the
-#' feature selection is returned. Otherwise the type can be set to "modeltraining"
-#' to return the set of IDs that are used for modeltraining.
-#' @param numPartitions The number of partitions that are build for feature selection
-#' @param seed The possibility to change the seed, for different results in the
-#' part of the ID shuffling for feature selection.
+#' @param phenotypeIDs A data.frame with samples as rows and sample IDs as row
+#'   names. Columns are phenotypes of interest. If for a sample no information
+#'   about a phenotype is available, it has to be indicated by NA.
+#' @param dataIDs A data.frame with samples as rows and the data modalities as
+#'   columns. Row names represent sample IDs. It holds the data IDs of a sample
+#'   for each modality. If for a sample there is no data for a modality, it has
+#'   to be indicated by NA.
+#' @param phenotype The name of the column in phenotypeIDs, which will be used
+#'   in the analysis.
+#' @param partitioning A value, that determines what percentage of samples are
+#'   used for training during feature selection and model training.
+#' @param numPartitions The number of partitions that are build for feature
+#'   selection and model training.
+#' @param k The amount of folds used for k-fold cross validation during model
+#'   training.
+#' @param iter The amount of iterations for k-fold cross validation during model
+#'   training.
+#' @param seed The seed used for random number generation. Using the same seed
+#'   ensures reproducibility.
 #'
-#' @return The return of the function can be three different outcomes depending
-#' on the selected type. The return is always a list containing the IDs for feature
-#' selection or modeltraining.
+#' @return The function return three lists, each containing partitions of the
+#'   sample IDs for feature selection testing/training and model training
+#'   respectively.
 #'
-#' @author Ulrich Asemann
+#' @author Ulrich Asemann & Wilhelm Glaas
 
 DataPartitioning <-
   function(phenotypeIDs,
            dataIDs,
+           phenotype,
            partitioning = 0.8,
            numPartitions = 100,
-           type = "training",
+           k = 5,
+           iter = 100,
            seed = 123) {
-    # Set seed, to achieve same results
+    # Setting seed
     set.seed(seed)
 
     # Defining variables
-    tmpList <- list()
-    tmpList2 <- list()
     trainingIDs <- list()
-    singleFeatureList <- list()
-    featureList <- list()
-    returnList <- list()
+    trainSingleFeatureList <- list()
+    testSingleFeatureList <- list()
+    testFeatureList <- list()
+    trainFeatureList <- list()
+    finalList <- list()
 
     # List of feature selection modalities
     listNames <- colnames(dataIDs)
 
     # Selecting IDs used for training and feature selection
     IDs <-
-      phenotypeIDs %>% dplyr::select(inc3) %>% drop_na() %>% rownames() %>% as.double()
+      phenotypeIDs %>%
+        dplyr::select(any_of(phenotype)) %>%
+        drop_na() %>%
+        rownames() %>%
+        as.double()
+
+    # save row names as a column
+    dataIDs <- dataIDs %>% tibble::rownames_to_column(var = "sampleID")
 
     # Data frame with the selected IDs
-    newSampleIDs <- dataIDs %>% filter(Clinical %in% IDs)
+    newSampleIDs <- dataIDs %>% filter(sampleID %in% IDs)
 
 
     # Building the model training IDs list
-    # Selecting clinical IDs with IDs for each modality
+
+    # Selecting sample IDs with IDs for each modality
     trainingSampleIDs <-
-      newSampleIDs %>% drop_na() %>% dplyr::select(Clinical)
+      newSampleIDs %>% drop_na() %>% dplyr::select(sampleID)
 
 
     # Building a list with the modeltraining IDs
-    for (i in 1:nrow(trainingSampleIDs)) {
-      trainingIDs <- c(trainingIDs, trainingSampleIDs[i, ])
-    }
+    trainingIDs <- trainingSampleIDs$sampleID
 
-    if (type == "modeltraining") {
-      # Create partitions for the modeltraining
-      dataPartitions <-
-        caret::createDataPartition(unlist(trainingSampleIDs),
-                                   times = 100,
-                                   p = 0.8)
+    # Create partitions for the modeltraining
+    kFolds <-
+      caret::createMultiFolds(trainingIDs,
+                                 times = iter,
+                                 k = k)
+    returnList <- list()
+    for (i in 0:(iter - 1)) {
+      tmpFoldList <- list()
+      for (j in 1:k) {
+        # Creating the training and testing lists for each fold
+        tmpIds <- kFolds[[i * k + j]]
+        trainIdsFold <- trainingIDs[tmpIds]
+        testIdsFold <- trainingIDs[-tmpIds]
 
-      # Create the partitions with the clinical IDs
-      for (i in 1:length(dataPartitions)) {
-        # Select the IDs
-        tmpList <- trainingIDs[unlist(dataPartitions[i])]
-
-        # Build the lists holding the IDs
-        partitionTrainingIDs <-
-          split(tmpList, cut(seq_along(tmpList), 4, labels = FALSE))
-        partitionTestingIDs <-
-          trainingIDs[-unlist(dataPartitions[i])]
-
-        # Putting the lists together
-        tmpList <-
-          list(c("Fold" = partitionTrainingIDs,
-                 list("Fold.5" = partitionTestingIDs)))
-
-        returnList <- c(returnList, tmpList)
-        names(returnList)[i] <- paste("Modeltraining", i)
-
+        tmpList <- list('Training' = trainIdsFold, 'Testing' = testIdsFold)
+        tmpFoldList[[paste0('Fold', j)]] = tmpList
       }
-      # Return the result
-      returnList <- list("Modeltraining" = returnList)
-
-      return(returnList)
+      returnList[[paste0('Iteration', i+1)]] = tmpFoldList
     }
+
+    # append to the final list
+    finalList <- c(finalList, "Modeltraining" = list(returnList))
+
 
     # Remove the modeltraining IDs from the newSampleIDs
-    removeIDs <- unlist(trainingIDs)
     newSampleIDs <-
-      newSampleIDs %>% filter(!(Clinical %in% removeIDs))
+      newSampleIDs %>% filter(!(sampleID %in% trainingIDs))
+
 
     # Building a feature selection list for each modality
     for (i in 1:length(listNames)) {
       # Select one feature and remove all NA values from the newSampleIDs list
       frame <-
-        newSampleIDs %>% dplyr::select(listNames[i], Clinical) %>% drop_na() %>% dplyr::select(Clinical)
+        newSampleIDs %>% dplyr::select(listNames[i]) %>% drop_na()
 
       # Creating data partitions with "caret"
       dataPartitions <-
-        caret::createDataPartition(unlist(frame), times = numPartitions, p = partitioning)
+        caret::createDataPartition(unlist(frame),
+                                   times = numPartitions, p = partitioning)
 
-      # Go through all 100 partitions
+      # Iterating over the partitions
       for (j in 1:length(dataPartitions)) {
-        # Build the feature selection lists, depending on training or testing
-        if (type == "testing") {
-          tmpFrame <- frame[-(unlist(dataPartitions[j])), ]
-        } else if (type == "training") {
-          tmpFrame <- frame[(unlist(dataPartitions[j])), ]
-        } else {
-          return("Please set the type to testing or training!")
-        }
 
-        # Building the list of the current partition
-        for (k in 1:nrow(tmpFrame)) {
-          tmpList <- c(tmpList, tmpFrame[k, ])
-          newName <- paste(listNames[i], j, k)
-          names(tmpList)[k] <- newName
-        }
+        # Build the feature selection lists
+        testTmpList <- unlist(frame[-dataPartitions[[j]], ])
+        trainTmpList <- unlist(frame[dataPartitions[[j]], ])
 
-        # Adding the tmpList to the singleFeatureList
-        singleFeatureList[[j]] <- tmpList
-        names(singleFeatureList)[j] <- paste(listNames[i], j)
+        # Adding the tmpLists to the singleFeatureLists
+        testSingleFeatureList[[j]] <- unname(testTmpList)
+        names(testSingleFeatureList)[j] <- paste0(listNames[i], j)
 
-        # Empty the tmpList
-        tmpList <- list()
+        trainSingleFeatureList[[j]] <- unname(trainTmpList)
+        names(trainSingleFeatureList)[j] <- paste0(listNames[i], j)
 
       }
 
-      # Add the singleFeatureList to their respective feature
-      assign(listNames[i], singleFeatureList)
+      # Add the testSingleFeatureLists to their respective featureList
+      testFeatureList[i] <- list(testSingleFeatureList)
+      names(testFeatureList)[i] <- listNames[i]
+
+      trainFeatureList[i] <- list(trainSingleFeatureList)
+      names(trainFeatureList)[i] <- listNames[i]
+
     }
 
-    # Put all singleFeatureLists into one list
-    for (i in 1:length(listNames)) {
-      featureList[[i]] <- get(listNames[i])
-      names(featureList)[i] <- listNames[i]
-    }
-
-    # Put the modeltraining IDs list and feature selection lists into one
-    if (type == "testing") {
-      returnList <-
-        list("Testing Feature Selection IDs" = featureList)
-    } else if (type == "training") {
-      returnList <-
-        list("Training Feature Selection IDs" = featureList)
-    } else if (type == "modeltraining") {
-      returnList <- list("Modeltraining IDs" = trainingIDs)
-    }
+    finalList <- c(finalList,
+                   "Testing Feature Selection IDs" = list(testFeatureList),
+                   "Training Feature Selection IDs" = list(trainFeatureList))
 
     # Return the completed list
-    return(returnList)
+    return(finalList)
   }
